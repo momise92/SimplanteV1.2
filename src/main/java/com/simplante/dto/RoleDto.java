1724e91417c6d0cf5c4d62b7b7534cc55ec5e223
momise92 <boss973@hotmail.fr>
2019-04-25 01:14:25 +0200

    Create RoleDto.java

---
+++ src/main/java/com/simplante/dto/RoleDto.java
@@ -0,0 +1,18 @@
+package com.simplante.dto;
+
+
+public class RoleDto {
+
+    private String role;
+
+    public RoleDto() {
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
